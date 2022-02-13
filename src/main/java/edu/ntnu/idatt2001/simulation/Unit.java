a87b4924c0439d4461793f31c2584f7f999d2800
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-02-13 17:42:43 +0100

    feat: add Unit class with general structure
    Outline the general methods of the Unit class.

---
+++ src/main/java/edu/ntnu/idatt2001/simulation/Unit.java
@@ -0,0 +1,53 @@
+package edu.ntnu.idatt2001.simulation;
+
+public abstract class Unit {
+    private String name;
+    private int health;
+    private int attack;
+    private int armor;
+
+    public Unit(String name, int health, int attack, int armor) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    public void attack(Unit opponent){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public int getAttackBonus(){
+        return 0;
+    }
+
+    public int getResistBonus(){
+        return 0;
+    }
+
+}
