39a7f2da405af50e6ad69a8dec76e416f4ed6b14
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-02-15 15:47:41 +0100

    feat: add java files for the testing of the subclasses to Unit.

---
+++ src/test/java/edu/ntnu/trym/simulation/CommanderUnitTest.java
@@ -0,0 +1,4 @@
+package edu.ntnu.trym.simulation;
+
+public class CommanderUnitTest {
+}
---
+++ src/test/java/edu/ntnu/trym/simulation/RangedUnitTest.java
@@ -0,0 +1,10 @@
+package edu.ntnu.trym.simulation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RangedUnitTest {
+
+
+}
