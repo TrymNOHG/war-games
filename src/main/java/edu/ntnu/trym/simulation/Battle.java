06644db34b76a68e6566ffe9be6278593dd50927
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-02-27 13:43:38 +0100

    docs: add JavaDoc to the whole Battle class

--- src/main/java/edu/ntnu/trym/simulation/Battle.java
+++ src/main/java/edu/ntnu/trym/simulation/Battle.java
@@ -1,11 +1,21 @@
 package edu.ntnu.trym.simulation;
 
+/**
+ * This class represents a battle between two armies. It, therefore, thoroughly defines a simulation what
+ * this battle would look like and who the winner is.
+ */
 public class Battle {
     private Army armyOne;
     private Army armyTwo;
     private Army[] armyArr;
     private int armyFightCount;
 
+    /**
+     * This constructor creates a Battle object, with the necessary information such as what two armies are
+     * participating and who starts the battle {@link #randomStartArmy()}.
+     * @param armyOne An army participating in the battle, represented as the object Army
+     * @param armyTwo Another army participating in the battle, represented as the object Army
+     */
     public Battle(Army armyOne, Army armyTwo) {
         this.armyOne = armyOne;
         this.armyTwo = armyTwo;
@@ -13,10 +23,20 @@ public class Battle {
         this.armyFightCount = randomStartArmy();
     }
 
+    /**
+     * This method chooses a random army to start the battle, between 0-1.
+     * @return An int representing which army attacks first
+     */
     private int randomStartArmy(){
         return (int) Math.round(Math.random());
     }
 
+    /**
+     * This method enacts a battle between two armies. The armies take turns fighting and the two opponents are chosen
+     * at random from both armies. Once a Unit's health goes below 0, it is removed from its respective army. Once an
+     * army has lost all of its Units, the other army is declared the winner.
+     * @return The winning army, represented as an Army object
+     */
     public Army simulate(){
         //Choosing fighters
         Unit fightingOpponent = armyArr[armyFightCount % 2].getRandom();
