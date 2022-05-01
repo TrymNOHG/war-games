a069811ad6f4fb476748c0f0a0e56c8998ce6720
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-01 23:32:14 +0200

    feat: add an enum class for different terrains

---
+++ src/main/java/edu/ntnu/trym/simulation/TerrainType.java
@@ -0,0 +1,16 @@
+package edu.ntnu.trym.simulation;
+
+/**
+ * This enum class represents different types of terrains that a Unit may fight on; each yielding different advantages
+ * and disadvantages to a given unit. The DEFAULT enumeration represents no terrain.
+ */
+public enum TerrainType {
+    DEFAULT,
+    HILL,
+    PLAINS,
+    FOREST
+}
+
+//Maybe attach values to these enumerations
+//Should I have a default?
+
