5686644bfdd22f1fa578cba5e98235c7ccdef836
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-01 14:25:28 +0200

    feat: add enum for different unit types

---
+++ src/main/java/edu/ntnu/trym/simulation/UnitType.java
@@ -0,0 +1,20 @@
+package edu.ntnu.trym.simulation;
+
+//TODO: Javadoc
+public enum UnitType {
+    INFANTRY,
+    RANGED,
+    CAVALRY,
+    COMMANDER
+
+
+}
+
+//Would it be wise to attach the default attack and armor value to the enums? Would the coupling be too high?
+/*
+    INFANTRY(15, 10),
+    RANGED(15, 8),
+    CAVALRY(20, 12),
+    COMMANDER(25, 15)
+
+ */
