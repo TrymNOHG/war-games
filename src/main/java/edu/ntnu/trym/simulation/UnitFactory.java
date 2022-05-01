40416bad4be345a5dbe21753d99e38761438018f
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-01 15:07:02 +0200

    docs: create javadoc for the UnitFactory Class

--- src/main/java/edu/ntnu/trym/simulation/UnitFactory.java
+++ src/main/java/edu/ntnu/trym/simulation/UnitFactory.java
@@ -11,18 +11,36 @@ public class UnitFactory {
 
     //Use this when file handling for example
 
-//    public static Unit getUnit(UnitType type, String name, int healthValue){
-//        switch(type){
-//            case RANGED: return new RangedUnit(name, healthValue, );
-//            case CAVALRY: return new CavalryUnit(name, healthValue);
-//            case INFANTRY: return new InfantryUnit(name, healthValue);
-//            case COMMANDER: return new CommanderUnit(name, healthValue);
-//        }
-//        return null;
-//    }
+    /**
+     * This method takes in a UnitType and the simplified information for a unit in order to retrieve an instance of
+     * the unit.
+     * @param type          The type of unit, represented as a UnitType enumeration
+     * @param name          The name of the unit, given as a String
+     * @param healthValue   The health of the unit, represented as an integer
+     * @return              A Unit object with the information specified
+     */
+    public static Unit getUnit(UnitType type, String name, int healthValue){
+        return switch (type) {
+            case RANGED -> new RangedUnit(name, healthValue);
+            case CAVALRY -> new CavalryUnit(name, healthValue);
+            case INFANTRY -> new InfantryUnit(name, healthValue);
+            case COMMANDER -> new CommanderUnit(name, healthValue);
+        };
+    }
 
     //Use this when file handling for example
-    public static Unit getUnit(UnitType type, String name, int healthValue, int attackValue, int armorValue){
+
+    /**
+     * This method takes in a UnitType and the additional information for a unit in order to retrieve an instance of
+     * the specialized unit.
+     * @param type          The type of unit, represented as a UnitType enumeration
+     * @param name          The name of the unit, given as a String
+     * @param healthValue   The health of the unit, represented as an integer
+     * @param attackValue   The attack value of the unit, represented as an integer
+     * @param armorValue    The armor value of the unit, represented as an integer
+     * @return              A Unit object with the information specified
+     */
+    public static Unit getUnit(UnitType type, String name, int healthValue, int attackValue, int armorValue) {
         return switch (type) {
             case RANGED -> new RangedUnit(name, healthValue, attackValue, armorValue);
             case CAVALRY -> new CavalryUnit(name, healthValue, attackValue, armorValue);
@@ -40,6 +58,33 @@ public class UnitFactory {
 //    }
 
     //Use this when actually generating the army
+
+    /**
+     * This method creates a list of simplified units with the information below. The list will contain the amount
+     * specified in the parameters of the same unit.
+     * @param numberOfUnits     The number of units desired, given as an integer
+     * @param type              The type of unit, represented as a UnitType enumeration
+     * @param name              The name of the unit, given as a String
+     * @param healthValue       The health of the unit, represented as an integer
+     * @return                  An arrayList filled with the same unit with the given length
+     */
+    public static List<Unit> getMultipleUnits(int numberOfUnits, UnitType type, String name, int healthValue){
+        List<Unit> listToBeReturned = new ArrayList<>();
+        IntStream.range(0, numberOfUnits).forEach(i -> listToBeReturned.add(getUnit(type, name, healthValue)));
+        return listToBeReturned;
+    }
+
+    /**
+     * This method creates a list of specialized units with the information below. The list will contain the amount
+     * specified in the parameters of the same unit.
+     * @param numberOfUnits     The number of units desired, given as an integer
+     * @param type              The type of unit, represented as a UnitType enumeration
+     * @param name              The name of the unit, given as a String
+     * @param healthValue       The health of the unit, represented as an integer
+     * @param attackValue       The attack value of the unit, represented as an integer
+     * @param armorValue        The armor value of the unit, represented as an integer
+     * @return                  An arrayList filled with the same unit with the given length
+     */
     public static List<Unit> getMultipleUnits(int numberOfUnits, UnitType type, String name, int healthValue,
                                               int attackValue, int armorValue){
         List<Unit> listToBeReturned = new ArrayList<>();
