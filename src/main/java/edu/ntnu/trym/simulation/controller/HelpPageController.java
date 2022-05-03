68c4815ec71edc79ac72606dfb808d38e5b4e6e1
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-03 20:11:14 +0200

    feat: add help page controller

---
+++ src/main/java/edu/ntnu/trym/simulation/controller/HelpPageController.java
@@ -0,0 +1,23 @@
+package edu.ntnu.trym.simulation.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class HelpPageController {
+    @FXML
+    private Text helpPageHeader;
+
+    @FXML
+    private Text informationText;
+
+    @FXML
+    void nextPage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void previousPage(MouseEvent event) {
+
+    }
+}
