0ff721a4ca90a5c7782b75f52f610acca4070fcd
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-05 16:31:19 +0200

    feat: add link back to unit information

--- src/main/java/edu/ntnu/trym/simulation/controller/BattlePrepController.java
+++ src/main/java/edu/ntnu/trym/simulation/controller/BattlePrepController.java
@@ -2,10 +2,24 @@ package edu.ntnu.trym.simulation.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 
 public class BattlePrepController {
+
+
+    @FXML
+    private Text armyNameText1;
+
+    @FXML
+    private Text armyNameText12;
+
+    @FXML
+    private Text orText;
+
+    @FXML
+    private Text orText2;
 
     @FXML
     void backToMainMenu(ActionEvent event) throws IOException {
@@ -13,7 +27,12 @@ public class BattlePrepController {
     }
 
     @FXML
-    void continueButton(ActionEvent event) {
+    void continueButton(ActionEvent event) throws IOException {
         SceneHandler.loadBattlePreparation(event);
+    }
+
+    @FXML
+    void backToUnitInfo(ActionEvent event) throws IOException {
+        SceneHandler.loadUnitInformation(event);
     }
 }
