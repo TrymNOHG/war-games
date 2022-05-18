c700c0c3cd1e9cd7d71df4f399842875fba2703e
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-18 16:27:23 +0200

    refactor: reorganize nodes for saved armies in scenebuilder

--- src/main/java/edu/ntnu/trym/simulation/controller/SavedArmyController.java
+++ src/main/java/edu/ntnu/trym/simulation/controller/SavedArmyController.java
@@ -1,14 +1,45 @@
 package edu.ntnu.trym.simulation.controller;
 
+import edu.ntnu.trym.simulation.model.ArmyFileDisplay;
+import edu.ntnu.trym.simulation.model.filehandling.ArmyFileHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SavedArmyController implements Initializable {
 
+    @FXML
+    private BorderPane saveSlot1;
+
+    @FXML
+    private BorderPane saveSlot2;
+
+    @FXML
+    private BorderPane saveSlot3;
+
+    @FXML
+    private BorderPane saveSlot4;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Hello");
+        VBox vBox;
+        try {
+            vBox = new ArmyFileDisplay
+                    (new File(new ArmyFileHandler().getFileSourcePath("Test"))).getArmyDisplay();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        saveSlot4.setCenter(vBox);
+//        saveSlot1.setCenter(new Text("Hello"));
 
     }
 
