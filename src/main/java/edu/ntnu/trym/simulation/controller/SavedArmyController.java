c99eae995525beb69b18d7931593d39092ae5cbb
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-18 14:39:07 +0200

    feat: add controller for SavedArmy scene

---
+++ src/main/java/edu/ntnu/trym/simulation/controller/SavedArmyController.java
@@ -0,0 +1,15 @@
+package edu.ntnu.trym.simulation.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SavedArmyController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+}
