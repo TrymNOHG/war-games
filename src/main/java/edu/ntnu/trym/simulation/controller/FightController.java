d3ce9667ab34a8b2f0a565925b1db93945caf774
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-20 13:46:43 +0200

    feat: add method for setting up and conducting the battle

--- src/main/java/edu/ntnu/trym/simulation/controller/FightController.java
+++ src/main/java/edu/ntnu/trym/simulation/controller/FightController.java
@@ -1,4 +1,29 @@
 package edu.ntnu.trym.simulation.controller;
 
-public class FightController {
+import edu.ntnu.trym.simulation.model.Army;
+import edu.ntnu.trym.simulation.model.Battle;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FightController implements Initializable {
+    private Battle battle;
+
+    private Army winnerArmy;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initialData();
+        conductBattle();
+    }
+
+    private void initialData(){
+        this.battle = new Battle(SimulationSingleton.INSTANCE.getArmy1(), SimulationSingleton.INSTANCE.getArmy2(),
+                SimulationSingleton.INSTANCE.getCurrentTerrain());
+    }
+
+    private void conductBattle(){
+        this.winnerArmy = this.battle.simulate();
+
+    }
 }
