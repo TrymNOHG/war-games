24088362f8614039cf4736b833c8fccdaa2eecc7
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-21 14:38:41 +0200

    feat: add button from results back to battle preparations

--- src/main/java/edu/ntnu/trym/simulation/controller/FightController.java
+++ src/main/java/edu/ntnu/trym/simulation/controller/FightController.java
@@ -6,6 +6,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,26 +21,49 @@ public class FightController implements Initializable {
 
     private Army winnerArmy;
 
+    @FXML
+    private ImageView iconArmy1;
 
+    @FXML
+    private ImageView iconArmy2;
+
+    @FXML
+    private Pane resultsArmy1;
+
+    @FXML
+    private Pane resultsArmy2;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         initialData();
-        conductBattle();
+        if(this.battle == null) initiateFight();
+
     }
 
     @FXML
     void skipFight(ActionEvent event) throws IOException {
-        SceneHandler.loadFightScreen(event);
+        SceneHandler.loadResultScreen(event);
+    }
+
+
+    @FXML
+    void backToBattlePrep(ActionEvent event) throws IOException {
+        SceneHandler.loadBattlePreparation(event);
     }
 
     private void initialData(){
+        this.battle = SimulationSingleton.INSTANCE.getBattle();
+    }
+
+    private void initiateFight(){
         this.battle = new Battle(SimulationSingleton.INSTANCE.getArmy1(), SimulationSingleton.INSTANCE.getArmy2(),
                 SimulationSingleton.INSTANCE.getCurrentTerrain());
+        conductBattle();
     }
 
     private void conductBattle(){
         this.winnerArmy = this.battle.simulate();
+        SimulationSingleton.INSTANCE.setBattle(this.battle);
         this.skipButton.setDisable(false);
         this.skipButton.setId("main-load-button");
     }
