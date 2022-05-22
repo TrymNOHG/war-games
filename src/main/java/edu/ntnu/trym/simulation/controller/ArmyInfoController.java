361e846ab5ceec94d8cffa2cde78f248500eaef6
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-22 22:45:48 +0200

    feat: create method for setting army information to pane

--- src/main/java/edu/ntnu/trym/simulation/controller/ArmyInfoController.java
+++ src/main/java/edu/ntnu/trym/simulation/controller/ArmyInfoController.java
@@ -1,8 +1,15 @@
 package edu.ntnu.trym.simulation.controller;
 
+import edu.ntnu.trym.simulation.model.armydisplay.ArmyDisplay;
+import edu.ntnu.trym.simulation.model.units.Unit;
+import edu.ntnu.trym.simulation.model.units.UnitType;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
 
 public class ArmyInfoController {
 
@@ -10,7 +17,32 @@ public class ArmyInfoController {
     private Pane informationPane;
 
     @FXML
-    void backToBattlePrep(ActionEvent event) {
+    private TableView<Unit> armyTable;
+
+    public void initialize(){
+        setArmyInformation();
+        setArmyTable();
+    }
+
+    @FXML
+    void backToBattlePrep(ActionEvent event) throws IOException {
+        SceneHandler.loadBattlePreparation(event);
+    }
+
+    private void setArmyInformation(){
+        VBox armyVBox = new ArmyDisplay.Builder(SimulationSingleton.INSTANCE.getArmyOfCurrentArmy())
+                .addArmyName()
+                .addAmountOfUnitInformation()
+                .addUnitInformation(UnitType.INFANTRY)
+                .addUnitInformation(UnitType.CAVALRY)
+                .addUnitInformation(UnitType.RANGED)
+                .addUnitInformation(UnitType.COMMANDER)
+                .build();
+
+        this.informationPane.getChildren().setAll(armyVBox);
+    }
+
+    private void setArmyTable(){
 
     }
 
