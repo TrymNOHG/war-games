8da346b1ee10b54574d9375acfaffe57194b371e
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-22 22:35:38 +0200

    feat: add controller for army information scene

---
+++ src/main/java/edu/ntnu/trym/simulation/controller/ArmyInfoController.java
@@ -0,0 +1,11 @@
+package edu.ntnu.trym.simulation.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class ArmyInfoController {
+
+    @FXML
+    private Pane informationPane;
+
+}
