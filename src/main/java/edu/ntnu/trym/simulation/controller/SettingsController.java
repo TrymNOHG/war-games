7bd02966c78ded63260d76f245b08e9ff1c4e614
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-22 20:18:11 +0200

    docs: add deprecated annotation as well as javadocs

--- src/main/java/edu/ntnu/trym/simulation/controller/SettingsController.java
+++ src/main/java/edu/ntnu/trym/simulation/controller/SettingsController.java
@@ -1,5 +1,9 @@
 package edu.ntnu.trym.simulation.controller;
 
+/**
+ * There wasn't enough time, as well as enough functions, to create a reasonable settings.
+ */
+@Deprecated
 public class SettingsController {
 
 }
