6612744c08be4b05626c80fc19be823161117120
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-22 21:56:43 +0200

    docs: add javadocs over the UnitType enum

--- src/main/java/edu/ntnu/trym/simulation/model/units/UnitType.java
+++ src/main/java/edu/ntnu/trym/simulation/model/units/UnitType.java
@@ -1,6 +1,10 @@
 package edu.ntnu.trym.simulation.model.units;
 
-//TODO: Javadoc
+/**
+ * This enumeration represents the different types of units that exist: infantry, ranged, cavalry, and commander.
+ *
+ * @author Trym Hamer Gudvangen
+ */
 public enum UnitType {
     INFANTRY,
     RANGED,
