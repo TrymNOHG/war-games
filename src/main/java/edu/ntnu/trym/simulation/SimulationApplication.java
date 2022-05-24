c9e8c5309b6e0d35faf2f77ca548fab297cad30d
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-05-24 06:13:43 +0200

    feat: add separate class for starting simulation

---
+++ src/main/java/edu/ntnu/trym/simulation/SimulationApplication.java
@@ -0,0 +1,17 @@
+package edu.ntnu.trym.simulation;
+
+import edu.ntnu.trym.simulation.controller.MainMenuController;
+
+/**
+ * This class acts as the starting point for the whole application.
+ */
+public class SimulationApplication {
+
+    /**
+     * The method that is called during run-time to start the applications.
+     * @param args  The arguments that are used to launch the application.
+     */
+    public static void main(String[] args) {
+        MainMenuController.main(args);
+    }
+}
